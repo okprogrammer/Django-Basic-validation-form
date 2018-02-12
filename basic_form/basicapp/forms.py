57243d621d36c84b64ff6393ef5ccf7242e90ec9
okprogrammmer <omk7912@gmail.com>
2018-02-13 00:41:30 +0530

    verify email

--- basic_form/basicapp/forms.py
+++ basic_form/basicapp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField(max_length=252)
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Make Sure Email Match!")
